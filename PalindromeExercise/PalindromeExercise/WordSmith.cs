4d85996d6bd1052e65abea6c40f5eddea8acfdad
jmmascorro <jessemascorro88@gmail.com>
2022-03-09 07:38:58 -0600

    FinishedPalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            var reversed = new string(charArray);
+
+            if(reversed == text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("radar", true)]
+        [InlineData("hello", false)]
+        [InlineData("truecoders", false)]
+        public void IsAPalindrome(string text, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(text);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
